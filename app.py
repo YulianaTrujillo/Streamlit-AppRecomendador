c4d6655b15aba3e18ab27c1712f3f6956b4e446a
YulianaTrujillo <120426595+YulianaTrujillo@users.noreply.github.com>
2025-12-04 23:08:57 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import pandas as pd
+from recommender import df, nn, X_emb, recommend_by_track_id
+
+st.set_page_config(page_title="Music Recommender", layout="centered")
+
+st.title("Recomendador de Música")
+st.write("Busca una canción y obtén recomendaciones similares usando tu modelo optimizado.")
+
+
+# ------------------------------------------------
+# BUSCADOR DE CANCIONES
+# ------------------------------------------------
+st.subheader("Buscar canción")
+
+query = st.text_input("Escribe parte del nombre de la canción:")
+
+selected_track_id = None
+
+if query:
+    df_ui = df[['track_id','track_name','artists','track_genre']].drop_duplicates(subset=['track_name','artists'])
+    filtradas = df_ui[df_ui["track_name"].str.contains(query, case=False, na=False)]
+
+    #filtradas = df[df["track_name"].str.contains(query, case=False, na=False)]
+
+    if filtradas.empty:
+        st.warning("No se encontraron canciones.")
+    else:
+        st.success(f"Se encontraron {len(filtradas)} canciones")
+
+        # Crear una columna combinando nombre y artista
+        filtradas["display_name"] = filtradas["track_name"] + " — " + filtradas["artists"] + " - " + filtradas["track_genre"]  
+
+        # Crear selectbox para que el usuario elija una
+        selected_song = st.selectbox(
+            "Selecciona la canción:",
+            options=filtradas["display_name"].tolist()
+        )
+
+        # Obtener track_id interno según la selección
+        selected_track_id = filtradas.loc[
+            filtradas["display_name"] == selected_song, "track_id"
+        ].values[0]
+
+
+        st.info(f"Track ID seleccionado automáticamente: `{selected_track_id}`")
+         # Mostrar info de la canción base
+        row = df[df["track_id"] == selected_track_id].iloc[0]
+        st.subheader("🎧 Canción seleccionada")
+        st.write(f"**{row['track_name']}** — {row['artists']}")
+        st.write(f"**Género:** {row['track_genre']}")
+        st.write("---")
+        
+# ------------------------------------------------
+# BOTÓN PARA RECOMENDAR
+# ------------------------------------------------
+st.subheader("Obtener recomendaciones")
+
+if st.button("Recomendar"):
+    if not selected_track_id:
+        st.error("Debes buscar una canción y seleccionarla.")
+    else:
+        try:
+            recs = recommend_by_track_id(selected_track_id, nn, df, X_emb,k_fixed=3, k_random=7, top_N=50)
+
+            st.success("Recomendaciones generadas:")
+
+            st.dataframe(recs, height=350)
+
+        except Exception as e:
+            st.error(f"Error: {str(e)}")
---
+++ recommender.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import numpy as np
+import joblib
+import requests
+import os
+import streamlit as st
+# ------------------------------------------------
+# CARGA DEL MODELO Y LOS DATOS
+# --------------------------------------
+# URLs se leerán desde los secrets de Streamlit Cloud
+MODEL_URL = st.secrets["MODEL_URL"]
+DF_URL = st.secrets["DF_URL"]
+
+MODEL_PATH = "model/nn_cosine_model.joblib"
+DF_PATH = "data/df_weighted.csv"
+
+@st.cache_resource
+def load_model():
+    if not os.path.exists(MODEL_PATH):
+        os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
+        with requests.get(MODEL_URL, stream=True) as r:
+            r.raise_for_status()
+            with open(MODEL_PATH, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+    return joblib.load(MODEL_PATH)
+
+@st.cache_resource
+def load_df():
+    if not os.path.exists(DF_PATH):
+        os.makedirs(os.path.dirname(DF_PATH), exist_ok=True)
+        with requests.get(DF_URL, stream=True) as r:
+            r.raise_for_status()
+            with open(DF_PATH, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+    return pd.read_csv(DF_PATH)
+
+
+
+#df = pd.read_csv("df_weighted.csv")
+#nn = joblib.load("nn_cosine_model.joblib")
+
+#Seleccion de variables para pasarlas por el modelo
+feature_cols = [c for c in df.columns if c not in 
+                ["track_id", "track_name", "artists", "album_name", "track_genre"]]
+
+X_emb = df[feature_cols].to_numpy()
+
+# ------------------------------------------------
+# FUNCIÓN DE RECOMENDACIÓN PRINCIPAL
+# ------------------------------------------------
+#Función principal para elegir el top de canciones dado de track_id de una canción
+def recommend_by_track_id(track_id, nn, df, X_emb, k_fixed=3, k_random=7, top_N=50, seed=None):
+
+    """
+      - k_fixed: top más similares, ordenados por similitud (deterministas)
+      - k_random: canciones tomadas aleatoriamente del top_N
+      - top_N: número de candidatos del cual muestrearemos los aleatorios
+
+    total top_k = k_fixed + k_random (por defecto = 10)
+    """
+
+    # --- 1. Buscar índice por track_id ---
+    mask = df["track_id"] == track_id
+    if not mask.any():
+        raise ValueError(f"track_id '{track_id}' no existe en el dataset.")
+
+    index = df.index[mask][0]        # label del df
+    pos = df.index.get_loc(index)    # posición en X_emb
+
+    # --- 2. Vector de características ---
+    vec = X_emb[pos].reshape(1, -1)    
+    n_retrieve = max(top_N, k_fixed + k_random)
+
+
+    # --- 3. Obtener vecinos ---
+    distances, indices = nn.kneighbors(vec, n_neighbors=n_retrieve)
+    distances = distances.flatten()
+    indices = indices.flatten()
+
+    # --- convertir distancia a similitud
+    metric = getattr(nn, "effective_metric_", getattr(nn, "metric", None))
+    if metric == "cosine":
+        similitudes = 1 - distances
+    else:
+        similitudes = 1 / (1 + distances)
+
+    # --- 4. Crear recomendaciones ---
+    resultados = []
+    for dist, p, sim in zip(distances, indices, similitudes):
+        row_label = df.index[p]
+        tid = df.loc[row_label, "track_id"]
+        if tid == track_id: 
+           continue
+        resultados.append({
+            "track_id": tid,
+            "track_name": df.loc[row_label, "track_name"],
+            "artists": df.loc[row_label, "artists"],
+            "track_genre": df.loc[row_label, "track_genre"],
+            "similitud": round(float(sim), 4)
+        })
+
+    # --- Top final limpio ---
+    resultados_df = pd.DataFrame(resultados)
+    resultados_df = resultados_df.drop_duplicates(subset=["track_name", "artists"], keep="first").reset_index(drop=True)
+    #resultados_df = resultados_df.head(top_k).reset_index(drop=True)
+
+    # asegurar que hay suficientes candidatos
+    if resultados_df.empty:
+        return resultados_df
+
+    # ORDENAR por similitud
+    resultados_df = resultados_df.sort_values(by="similitud", ascending=False).reset_index(drop=True)
+
+    # --- 1) TOP fijos ---
+    fixed_part = resultados_df.head(k_fixed)
+
+    # --- 2) RANDOM desde el resto del top_N ---
+    rest_pool = resultados_df.iloc[k_fixed:top_N]
+
+    rng = np.random.default_rng(seed)
+
+    if len(rest_pool) >= k_random:
+        random_idx = rng.choice(rest_pool.index, size=k_random, replace=False)
+        random_part = resultados_df.loc[random_idx]
+    else:
+        # fallback: tomar todo lo que haya
+        random_part = rest_pool
+
+    # unir ambas partes
+    final_df = pd.concat([fixed_part, random_part]).reset_index(drop=True)
+
+
+    return final_df
